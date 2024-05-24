f8701eb678b664b851813ff0891788920014d163
GitHub Action <action@github.com>
2024-05-24 06:22:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/1213999170/ideaplugininternalizationmessage/MyBundle.kt
+++ src/main/kotlin/com/github/1213999170/ideaplugininternalizationmessage/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.1213999170.ideaplugininternalizationmessage
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/1213999170/ideaplugininternalizationmessage/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/1213999170/ideaplugininternalizationmessage/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.1213999170.ideaplugininternalizationmessage.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
